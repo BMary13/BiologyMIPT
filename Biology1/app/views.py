922e5e4b5f0fd619a799026a80d2ecd34777999c
BMary13 <bruttan.mv@phystech.edu>
2018-03-11 10:35:16 +0300

    ///

---
+++ Biology1/app/views.py
@@ -0,0 +1,62 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home',
+            'year':datetime.now().year,
+        }
+    )
+
+def blog(request):
+    """Renders the blog page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/blog.html',
+        {
+            'title':'Blog about Genetics',
+            'message':'Posts about genetics.',
+            'year':datetime.now().year,
+        }
+    )
+
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'Biology sections',
+            'message':' ',
+            'year':datetime.now().year,
+        }
+    )
+
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Сontact',
+            'message':'My contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
